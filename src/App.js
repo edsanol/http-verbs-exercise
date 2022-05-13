97e4d548d7f7ccea49f124b3d5c425e95636fa66
Edinael <edsanol99@gmail.com>
2022-05-13 13:01:19 -0500

    Add HTTP verbs exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+
+axios.get(`https://jsonplaceholder.typicode.com/todos`)
+  .then(response => {
+    console.log('Respuesta GET', response.data)
+    console.log(response.status)
+  })
+
+axios.get(`https://jsonplaceholder.typicode.com/todos/2`)
+  .then(response => {
+    console.log('Respuesta GET id', response.data)
+    console.log(response.status)
+  })
+
+const newObj = {
+  userId: 1500,
+  title: 'Hello World',
+  completed: true,
+}
+
+axios.post(`https://jsonplaceholder.typicode.com/todos`, newObj)
+  .then(response => {
+    console.log('Respuesta POST', response.data)
+    console.log(response.status)
+  })
+
+axios.put(`https://jsonplaceholder.typicode.com/todos/1`, newObj)
+  .then(response => {
+    console.log('Respuesta PUT', response.data)
+    console.log(response.status)
+  })
+
+axios.delete(`https://jsonplaceholder.typicode.com/todos/1`)
+  .then(response => {
+    console.log('Respuesta DELETE', response.data)
+    console.log(response.status)
+  })
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Prueba
     </div>
   );
 }
